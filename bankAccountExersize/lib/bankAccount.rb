3c6dba2a45c4e054205e0ef61bfb5c4a658abdef
Michael Poole <michaelpoole10@gmail.com>
2021-07-25 23:56:48 +0100

    all the user stories are finished

--- bankAccountExersize/lib/bankAccount.rb
+++ bankAccountExersize/lib/bankAccount.rb
@@ -4,4 +4,14 @@ class BankAccount
   def deposit(amount)
     @balance = amount
   end
+
+  def withdraw
+    @money = @balance
+    @balance = 0
+    return @money
+  end
+
+  def viewBalance
+    return @balance
+  end
 end
--- bankAccountExersize/spec/bankAccount_spec.rb
+++ bankAccountExersize/spec/bankAccount_spec.rb
@@ -2,14 +2,34 @@ require "bankAccount"
 
 describe "BankAccount" do
   describe "#deposit" do
-  before do
-    @account = BankAccount.new
-  end
+    before do
+      @account = BankAccount.new
+    end
 
-  it "deposits money in the account" do
-    @account.deposit(5)
-    expect(@account.balance).to(eq(5))
+    it "deposits money in the account" do
+      @account.deposit(5)
+      expect(@account.balance).to(eq(5))
+    end
   end
+  describe "#withdraw" do
+    before do 
+      @account = BankAccount.new
+    end
 
+    it "withdraws money from the account" do
+      @account.deposit(5)
+      expect(@account.withdraw).to(eq(5))
+      expect(@account.balance).to(eq(0))
+    end
   end
+  describe "#viewBalance" do 
+    before do
+      @account = BankAccount.new
+      @account.deposit(5)
+    end
+
+    it "displays the balance in the account" do
+      expect(@account.viewBalance).to(eq(@account.balance))
+    end
+  end   
 end
